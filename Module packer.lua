280e29a5dec5419bd8c816b265880c750843a7a2
Nyxen <nil.nbeeww@gmail.com>
2024-09-10 01:41:26 +0400

    Update Module packer.lua

--- Module packer.lua
+++ Module packer.lua
@@ -19,69 +19,66 @@ local Services = {
 	'StarterPlayer'
 }
 
-
-local m = Instance.new("ModuleScript") 
+local m = Instance.new("ModuleScript")
 m.Parent = workspace
 m.Name = "MainModule"
 
-
-if game:GetService("StarterPlayer") then 
+if game:GetService("StarterPlayer") then
 	local sp = game:GetService("StarterPlayer"):WaitForChild("StarterPlayerScripts")
 	local sc = game:GetService("StarterPlayer"):WaitForChild("StarterCharacterScripts")
 	
-	local st = Instance.new("Folder") 
+	local st = Instance.new("Folder")
 	st.Parent = m
 	st.Name = "StarterPlayer"
 	
-	local xm = Instance.new("Folder") 
+	local xm = Instance.new("Folder")
 	xm.Parent = st
 	xm.Name = "StarterPlayerScripts"
 	
-	local o = Instance.new("Folder") 
+	local o = Instance.new("Folder")
 	o.Parent = st
 	o.Name = "StarterCharacterScripts"
 	
-	for _, v in pairs(sp:GetChildren()) do 
-		v.Parent = st
+	for _, v in pairs(sp:GetChildren()) do
+		v.Parent = xm
 	end
 	
 	for _, v in pairs(sc:GetChildren()) do
-		v.Parent = st
+		v.Parent = o
 	end
 end
 
 for _, v in pairs(Services) do
-	if v.Name ~= "StarterPlayer" then
-		local srv = game:GetService(v) 
-		if srv then 
-			local f = Instance.new("Folder") 
-			f.Parent = m 
-			f.Name = v 
-			for _, v in pairs(srv:GetChildren()) do 
+	if v ~= "StarterPlayer" then
+		local srv = game:GetService(v)
+		if srv then
+			local f = Instance.new("Folder")
+			f.Parent = m
+			f.Name = v
+			for _, v in pairs(srv:GetChildren()) do
 				if v.Name ~= 'Terrain' and v ~= f and v.Name ~= 'MainModule' then
-					v.Parent = f 
+					v.Parent = f
 				end
 			end
 		end
 	end
 end
 
-
 workspace['MainModule'].Source = [[
 
 local module = {}
 
-function module.load() 
-    for _, v in pairs(script:GetChildren()) do
-        pcall(function()
-            local service = game:GetService(v.Name)
-            if service then
-                for _, child in pairs(v:GetChildren()) do
-                    child.Parent = service
-                end
-            end
-        end)
-    end
+function module.load()
+	for _, v in pairs(script:GetChildren()) do
+		pcall(function()
+			local service = game:GetService(v.Name)
+			if service then
+				for _, child in pairs(v:GetChildren()) do
+					child.Parent = service
+				end
+			end
+		end)
+	end
 end
 
 return module
